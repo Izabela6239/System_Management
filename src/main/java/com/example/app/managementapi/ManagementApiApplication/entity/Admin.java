c98f3fec918a9fc98e6c368f8cbc27c158373fe5
trailas <sebastian.traila@gmail.com>
2025-10-21 15:55:26 +0300

    AI + Conexiune localhost + front

--- src/main/java/com/example/app/managementapi/ManagementApiApplication/entity/Admin.java
+++ src/main/java/com/example/app/managementapi/ManagementApiApplication/entity/Admin.java
@@ -49,4 +49,84 @@ public class Admin {
     // Relații cu rapoartele de productivitate
     @OneToMany(mappedBy = "admin")
     private List<MonthlyReport> reportsGenerated;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public List<Task> getManagedTasks() {
+        return managedTasks;
+    }
+
+    public void setManagedTasks(List<Task> managedTasks) {
+        this.managedTasks = managedTasks;
+    }
+
+    public List<LeaveRequest> getLeaveRequestsHandled() {
+        return leaveRequestsHandled;
+    }
+
+    public void setLeaveRequestsHandled(List<LeaveRequest> leaveRequestsHandled) {
+        this.leaveRequestsHandled = leaveRequestsHandled;
+    }
+
+    public List<Payroll> getPayrollsManaged() {
+        return payrollsManaged;
+    }
+
+    public void setPayrollsManaged(List<Payroll> payrollsManaged) {
+        this.payrollsManaged = payrollsManaged;
+    }
+
+    public List<MonthlyReport> getReportsGenerated() {
+        return reportsGenerated;
+    }
+
+    public void setReportsGenerated(List<MonthlyReport> reportsGenerated) {
+        this.reportsGenerated = reportsGenerated;
+    }
 }
